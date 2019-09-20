00e6da0663f1ec124616919732bf8828e40f6eea
Stefano Demiliani <33155438+demiliani@users.noreply.github.com>
2019-09-20 12:18:45 +0200

    Add files via upload

---
+++ BackupAzureSQLDatabase.ps1
@@ -0,0 +1,54 @@
+﻿# Sign in to Azure.
+Login-AzureRmAccount
+
+# Fill in your subscription and SQL Database details
+$resourceGroup = "YourAzureSqlDatabaseResourceGroup"
+$server = "YourAzureSqlServerName"
+$database = "YourAzureSqlDatabaseName"
+$sqlAdminLogin = "AdminUsername"
+$sqlPassword = "AdminPassword"
+
+# Custom Filename for the BACPAC
+$bacpacFilename = $database + (Get-Date).ToString("yyyy-MM-dd-HH-mm") + ".bacpac"
+#Storage account (container) URI
+$baseStorageUri = "https://StorageAccountName.blob.core.windows.net/BlobContainerName/"
+# URI for the final bacpac file
+$storageUri = $baseStorageUri + $bacpacFilename
+# Blob storage access key
+$storageKey= "YourStorageAccessKey"
+
+
+# Tenant ID from the account that created your AAD app:
+$tenantId = "YourTenantID"
+# This is the Application ID from your AAD app:
+$applicationId = "YourApplicationID"
+# This is the Secret from your AAD app:
+$key = ConvertTo-SecureString "YourAzureADAppPassword" -AsPlainText -Force
+
+# Acquire the authentication context
+$authUrl = "https://login.windows.net/${tenantId}"
+$authContext = [Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext]$authUrl
+$cred = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.ClientCredential $applicationId,$key
+$authresult = $authContext.AcquireToken("https://management.core.windows.net/",$cred)
+
+# Setting the request header with authentication and content type
+$authHeader = @{
+'Content-Type'='application/json'
+'Authorization'=$authresult.CreateAuthorizationHeader()
+}
+# Creation of the request body with storage details and database login
+$body = @{storageKeyType = 'StorageAccessKey'; `
+storageKey=$storageKey; `
+storageUri=$storageUri;`
+administratorLogin=$sqlAdminLogin; `
+administratorLoginPassword=$sqlPassword;`
+authenticationType='SQL'`
+} | ConvertTo-Json
+
+# REST URI to call
+$apiURI = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Sql/servers/$server/databases/$database/export?api-version=2014-04-01"
+
+# POST request to the REST API
+$result = Invoke-RestMethod -Uri $apiURI -Method POST -Headers $authHeader -Body $body
+
+Write-Output $result
---
+++ CreateAzureADApplication.ps1
@@ -0,0 +1,30 @@
+﻿# Sign in to Azure.
+Login-AzureRmAccount
+
+# If you have multiple subscriptions, uncomment and set to the subscription you want to work with:
+# $subscriptionId = "xxx"
+# Set-AzureRmContext -SubscriptionId $subscriptionId
+
+# Provide these values for your new Azure AD app:
+# $appName (display name for your app, must be unique in your directory)
+# $uri (it cannot be real)
+# $secret (password for your app)
+$appName = "yourapp"
+$uri = "http://yourapp"
+$secret = ConvertTo-SecureString "YourPassword" -AsPlainText -Force
+
+# Create the Azure AD app
+$azureAdApplication = New-AzureRmADApplication -DisplayName $appName -HomePage $Uri -IdentifierUris $Uri -Password $secret
+
+# Create a Service Principal for the app
+$svcprincipal = New-AzureRmADServicePrincipal -ApplicationId $azureAdApplication.ApplicationId
+
+# Assign the Contributor RBAC role to the service principal
+$roleassignment = New-AzureRmRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName $azureAdApplication.ApplicationId.Guid
+
+# Display the values for your application
+Write-Output "Save these values for using them in your application"
+Write-Output "Subscription ID:" (Get-AzureRmContext).Subscription.SubscriptionId
+Write-Output "Tenant ID:" (Get-AzureRmContext).Tenant.TenantId
+Write-Output "Application ID:" $azureAdApplication.ApplicationId.Guid
+Write-Output "Application Secret:" $secret
